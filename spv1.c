dea7201001fa76b66019452723539bcbda58e1c1
Chris Wagenaar <c.c.j.wagenaar@student.vu.nl>
2023-04-14 12:55:54 +0200

    recognized cache hit from array of 100

--- spv1.c
+++ spv1.c
@@ -13,16 +13,21 @@ int main(int argc, char* argv[]) {
 
     cp_t* arr = mmap_arr_cache_pages();
     printf("sizeof(arr) = %ld\n", sizeof(arr));
-    //print_mmap_arr_vals(arr);
+    print_mmap_arr_vals(arr);
 
-    volatile int _n_pages = 100; //afraid that constants will be propagated consts by the compiler, hence not in mem
+    volatile int n_pages = 100; //afraid that constants will be propagated consts by the compiler, hence not in mem
 
-    time_t t = time_mem_load((void*)&_n_pages);
-    printf("time = %ld\n", t);
-    flush((void*)&_n_pages);
-    t = time_mem_load((void*)&_n_pages);
-    printf("time = %ld\n", t);
+    //time_t t = time_mem_load((void*)&n_pages);
+    //printf("time = %ld\n", t);
+    //flush((void*)&n_pages);
+    //t = time_mem_load((void*)&n_pages);
+    //printf("time = %ld\n", t);
 
+    flush_arr((void*)arr);
 
+    volatile cp_t cp = arr[3];
+    printf("loaded cp 3. id = %d\n", cp.id);
+
+    reload(arr, n_pages);
 
 }
--- time_and_flush.c
+++ time_and_flush.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "gen_array.c"
+
 //NOTE: not sure if "static" works here
 static inline int time_mem_load(void *addr) {
     volatile unsigned long time;
@@ -28,6 +30,28 @@ static inline int time_mem_load(void *addr) {
 //NOTE: not sure if "static" works here
 static inline void flush(char *addr) {
     asm __volatile__ ("mfence\nclflush 0(%0)" : : "r" (addr) :);
+}
+
+void flush_arr(cp_t* arr) {
+
+    for(int i = 0; i < N_PAGES; i++) {
+        flush((void*)&arr[i]);
+    }
+
+}
+
+static inline void reload(cp_t *arr, int n_pages) {
+
+    for(int i = 0; i < n_pages; i++) {
+
+        time_t t = time_mem_load(&arr[i]);
+
+        printf("i=%d\ttime = %ld cycles\n", i, t);
+
+        if(t < 60) printf("\t\t\t\tCACHE\n");
+
+    }
+
 }
 
 #endif  //TIME_AND_FLUSH
